73067f248abe19de469b3573f0c87baf7f7ffb1f
ManarMahmoudAmin <manareltayeb825@gmail.com>
2024-03-21 16:03:48 +0200

    PL - MVC Template

--- Route.C41.G03.PL/Program.cs
+++ Route.C41.G03.PL/Program.cs
@@ -13,7 +13,9 @@ namespace Route.C41.G03.PL
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var hostBuilder = CreateHostBuilder(args).Build();
+
+            hostBuilder.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
