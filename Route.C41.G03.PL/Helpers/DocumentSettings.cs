60f8c1ffc1e558c0627ded24b500b4cf32f5e7e6
ManarMahmoudAmin <manareltayeb825@gmail.com>
2024-04-03 11:24:30 +0200

    Document Settings (Upload, Delete)

---
+++ Route.C41.G03.PL/Helpers/DocumentSettings.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.IO;
+
+namespace Route.C41.G03.PL.Helpers
+{
+    public class DocumentSettings
+    {
+        public static string UploadFile(IFormFile file, string folderName)
+        {
+            // 1. Get Located Folder Path
+            string folderPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\files", folderName);
+            if(!Directory.Exists(folderPath)) 
+                Directory.CreateDirectory(folderPath);
+
+            // 2. Get File Name And Make It UNIQUE
+            string fileName = $"{Guid.NewGuid()}{Path.GetExtension(file.FileName)}";
+
+            // 3. Get File Path
+            string filePath = Path.Combine(folderPath, fileName);
+
+            // 4. Save File As Streams
+            using var fileStream = new FileStream(filePath, FileMode.Create);
+            file.CopyTo(fileStream);
+            
+            // 5. Return File Name
+            return filePath;
+
+
+
+        }
+
+        public static void DeleteFile(string fileName, string folderName)
+        {
+            string filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\files", folderName, fileName);
+            if(File.Exists(filePath))
+                File.Delete(filePath);
+
+        }
+    }
+}
