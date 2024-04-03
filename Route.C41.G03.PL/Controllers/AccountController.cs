47105410d818185004cfb68c3495f5f4f6f6f224
ManarMahmoudAmin <manareltayeb825@gmail.com>
2024-04-03 16:28:34 +0200

    Account Controller - SignUp

---
+++ Route.C41.G03.PL/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Route.C41.G03.PL.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+    }
+}
