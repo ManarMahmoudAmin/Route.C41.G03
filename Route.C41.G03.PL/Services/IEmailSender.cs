7e1755fa2460cc96b378f83c6bb14e85a1e02ca1
ManarMahmoudAmin <manareltayeb825@gmail.com>
2024-04-25 16:47:14 +0200

    Send Email

---
+++ Route.C41.G03.PL/Services/IEmailSender.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Route.C41.G03.PL.Services
+{
+	public interface IEmailSender
+	{
+		Task SendAsync(string form, string recipients, string subject, string body);
+	}
+}
